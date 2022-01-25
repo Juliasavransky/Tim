937051280fabc4888b834cd63c3538c73c81912d
Juliasavransky <63636407+Juliasavransky@users.noreply.github.com>
2022-01-25 14:08:28 +0200

    Prepared for deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const connectDB = require('./config/db');
 const cors = require('cors');
 const app = express();
+const path = require('path');
 
 //Connect DB
 connectDB();
@@ -13,14 +14,23 @@ app.use(express.json({ extended: false }));
 app.use(cors());
 
 
-app.get('/', (req, res) => res.send('API testing'));
-
 //define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/orders', require('./routes/api/orders'));
 
+//serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+
+
+}
 
 
 
